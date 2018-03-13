d99a3d0917d8209224c0674ca17692f1c4c94701
Thian He <thianhe@hotmail.com>
2018-03-13 10:27:05 +0800

    Step 7-A

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -4,7 +4,17 @@
 using namespace System;
 bool Guess(int number)
 {
+    return false;
 }
 int main(array<System::String^>^ args)
 {
+    int guess;
+
+    do
+    {
+        std::cin >> guess;
+    }
+    while (!Guess(guess));
+
+    return 0;
 }
